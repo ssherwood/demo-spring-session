1038743e6827d160d9b41ed5c233af1bea6044a4
Shawn Sherwood <ssherwood@vmware.com>
2021-07-23 14:55:01 -0400

    Added jdbc support to compare to redis

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Demo Spring Session using Redis
+
+
+
+## Postgres
+
+Get the Postgres schema from here:
+https://github.com/spring-projects/spring-session/tree/master/spring-session-jdbc/src/main/resources/org/springframework/session/jdbc/
--- src/main/java/io/undertree/demos/springsession/SpringSessionDemoApplication.java
+++ src/main/java/io/undertree/demos/springsession/SpringSessionDemoApplication.java
@@ -2,14 +2,14 @@ package io.undertree.demos.springsession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.session.data.redis.config.ConfigureRedisAction;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.time.Instant;
 
 @SpringBootApplication
 public class SpringSessionDemoApplication {
@@ -25,12 +25,14 @@ public class SpringSessionDemoApplication {
 
     @Controller
     public class GreetingController {
+
         @GetMapping("/greeting")
         public String greeting(HttpSession session, @RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-            session.setAttribute("foo", "bar");
+            // put something new in the session
+            session.setAttribute("time_stamp", Instant.now());
+
             model.addAttribute("name", name);
             return "greeting";
         }
-
     }
 }
